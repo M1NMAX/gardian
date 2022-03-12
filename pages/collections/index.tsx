16e12d363a19d3480e7fa32577c3d1736fc59a82
M1NMAX <carlosaurora20@gmail.com>
2022-03-12 14:50:51 +0000

    added auth0 authentication

---
+++ pages/collections/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+const Collections: FC = () => {
+    return (
+        <div>Collections 
+            <a href="/api/auth/logout">Logout</a>
+        </div>
+    )
+}
+
+export default Collections
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
     <div className='text-red-500 uppercase text-center font-bold'>
-     <h1>hELLO</h1>
+      <h1>hELLO</h1>
+      <a href="/api/auth/login">Login</a>
     </div>
   )
 }
