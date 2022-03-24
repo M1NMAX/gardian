04b8b626c4f514a55fdda7158b9b8d14aa91a4d8
M1NMAX <carlosaurora20@gmail.com>
2022-03-24 12:37:02 +0000

    small upd to logo and collection overview component

--- components/CollectionOverview/index.tsx
+++ components/CollectionOverview/index.tsx
@@ -19,7 +19,7 @@ const Collecion: FC<CollectionOverviewProps> = ({ collection }) => {
             </Link>
             <span className='w-fit px-1 font-medium text-xs uppercase
             rounded border bg-gray-200  dark:bg-gray-700'>
-                {handleVariantName(collection.variant)} {collection.isSub?'yes': 'no'}
+                {handleVariantName(collection.variant)}
             </span>
             <span className='text-xs font-light italic'>
                 {collection.updatedAt ?
--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const Logo: FC = () => {
     return (
-        <div className='relative w-10 h-10 cursor-pointer'>
+        <div className='relative w-10 h-10'>
             <Image src={logoSrc} layout='fill' objectFit='contain' alt='Gardian logo' />
             <p className='ml-12 mt-1 text-2xl font-bold '>Gardian</p>
         </div>
