4be04f96c67365705af2015a864bb251ffa2d871
M1NMAX <carlosaurora20@gmail.com>
2022-04-02 14:46:14 +0100

    upd edit todo modal design

--- components/EditTodoModal/index.tsx
+++ components/EditTodoModal/index.tsx
@@ -14,30 +14,47 @@ const EditTodoModal: FC<EditTodoModalProps> = ({ todo, open, handleClose, positi
     const [isConcluded, setIsConcluded] = useState(todo.isConcluded)
     const [conclusionDate, setConclusionDate] = useState(todo.conclusionDate);
     const [reminder, setReminder] = useState(todo.reminder);
+    const [reminderDate, setReminderDate] = useState("")
+    const [description, setDescription] = useState("")
 
     return (
         <Modal title={<Label icon={<CheckIcon />} text="Todo" />} open={open} onHide={handleClose} >
             <form className='space-y-2'>
                 <div className='flex items-center space-x-2'>
-
                     <input type="checkbox"
                         name='taskStatus' checked={isConcluded} onChange={(e) => setIsConcluded(e.target.checked)}
-                        className='checkbox-input' />
+                        className='w-6 h-6 appearance-none  rounded-full bg-gray-200 dark:bg-gray-700 
+                        checked:bg-primary dark:checked:bg-primary-bright' />
+
                     <input type="text" name='name' value={name} onChange={(e) => setName(e.target.value)}
                         placeholder="Todo name"
                         className='modal-input' />
                 </div>
 
-                <div className='flex items-center space-x-2'>
 
-                    <button key={1} type="button" className='relative btn btn-secondary' onClick={() => setReminder(!reminder)}>
-                        <BellIcon className='icon-md' />
-                        {reminder && <BadgeCheckIcon className='absolute -top-1 -right-2 icon-xs text-primary' />}
-                    </button>
-                    <input type='date' name='ConclusionDate' value={conclusionDate}
-                        onChange={(e) => setConclusionDate(e.target.value)}
-                        className='modal-input' />
+
+
+                <div className='flex items-center space-x-2'>
+                    <label>
+                        <span className='text-sm'>Conclusion date </span>
+                        <input type='date' name='ConclusionDate' value={conclusionDate}
+                            onChange={(e) => setConclusionDate(e.target.value)}
+                            className='modal-input' />
+                    </label>
+                    <label>
+                        <span className='text-sm'>Reminder date </span>
+                        <input type='date' name='ConclusionDate' value={reminderDate}
+                            onChange={(e) => setReminderDate(e.target.value)}
+                            className='modal-input' />
+                    </label>
                 </div>
+
+                <label className="flex flex-col w-full mt-1">
+                    <span className='text-sm'> Description ({description.length}/200)</span>
+                    <textarea name='description' value={description} onChange={(e) => setDescription(e.target.value)}
+                        rows={4} maxLength={200}
+                        className='resize-none rounded border border-black bg-gray-50 dark:bg-gray-700' />
+                </label>
 
                 <div className="flex justify-end space-x-2">
                     <button onClick={handleClose} className="modal-neutral-btn">
--- components/Todos/index.tsx
+++ components/Todos/index.tsx
@@ -1,11 +1,13 @@
-import React, { FC, useEffect, useState } from 'react'
-import { BellIcon, CalendarIcon, TrashIcon } from '@heroicons/react/outline';
+import React, { FC, Fragment, useEffect, useState } from 'react'
+import { BellIcon, CalendarIcon, ChevronUpIcon, DocumentTextIcon, DotsVerticalIcon, PencilIcon, TrashIcon } from '@heroicons/react/outline';
 import { useRouter } from 'next/router'
 import { useQuery } from 'react-query';
 import { TodoInterface } from '../../interfaces';
 import EditTodoModal from '../EditTodoModal';
 import toast from 'react-hot-toast';
 import GenericMenu from '../GenericMenu';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import ActionIcon from '../ActionIcon';
 
 const Todos = () => {
     const router = useRouter();
@@ -55,18 +57,88 @@ const Todo: FC<TaskProps> = ({ todo }) => {
 
     return (
 
-        <div className='flex justify-between items-center space-x-2 px-2 py-1 rounded-md border group'>
+        <div className='flex justify-between items-start space-x-2 px-2 py-1 rounded-md border group'>
+            <label className='pt-1'>
+                <span className='sr-only'> Make as concluded </span>
+                <input type="checkbox" name='status'
+                    checked={taskStatus} onChange={handleCheck}
+                    className='checkbox-input' />
+            </label>
 
-            <input type="checkbox" name='status'
-                checked={taskStatus} onChange={handleCheck}
-                className='checkbox-input' />
+            <div className='grow'>
+                <button onClick={openEditTodoModal}
+                    className='w-full text-left'>
+                    <span>
+                        {todo.name}
+                    </span>
+                    <span className='flex space-x-2'>
+                        {todo.reminder && <span className='flex items-center text-xs'>
+                            <BellIcon className='icon-xs' /> tomorrow
+                        </span>}
+                        {todo.conclusionDate != '' && <span className='flex items-center space-x-0.5 text-xs'>
+                            <CalendarIcon className='icon-xs' />
+                            <span>
+                                {todo.conclusionDate && new Date(todo.conclusionDate).toDateString()}
+                            </span>
+                        </span>}
+                    </span>
+                </button>
+                <Disclosure>
+                    {({ open }) => (
+                        <>
+                            <Disclosure.Button className="flex justify-between space-x-0.5 w-fit p-1 text-sm font-medium text-left
+                              btn btn-secondary focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                                <DocumentTextIcon className='icon-xs' />
+                                <span>Description</span>
+                                <ChevronUpIcon
+                                    className={`${open ? 'transform rotate-180' : ''
+                                        } icon-xs`}
+                                />
+                            </Disclosure.Button>
+                            <Disclosure.Panel className="text-sm dark:text-white">
+                                If you're unhappy with your purchase for any reason, email us
+                                within 90 days and we'll refund you in full, no questions asked.
+                            </Disclosure.Panel>
+                        </>
+                    )}
+                </Disclosure>
 
-            <button onClick={openEditTodoModal}
-                className='grow text-left p-0'>
-                {todo.name}
-            </button>
+
+            </div>
             <div className='md:invisible md:group-hover:visible '>
-                <GenericMenu onClickRename={() => console.log("hh")} onClickDelete={() => console.log("hh")} />
+                <Menu as="div" className="relative" >
+                    <Menu.Button>
+                        <ActionIcon icon={<DotsVerticalIcon />} variant="secondary" />
+                    </Menu.Button>
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items as="ul" className="absolute  z-10 -right-2 w-fit p-1 rounded border  origin-top-right bg-white dark:bg-gray-900">
+                            <Menu.Item as="li">
+                                <button
+                                    className='w-full space-x-1 btn btn-secondary'>
+                                    <PencilIcon className='icon-sm' />
+                                    <span>
+                                        Rename
+                                    </span>
+                                </button>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <button
+                                    className='w-full space-x-1 btn btn-secondary'>
+                                    <TrashIcon className='icon-sm' />
+                                    <span> Delete </span>
+                                </button>
+                            </Menu.Item>
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
             </div>
             {editTodoModal && <EditTodoModal todo={todo}
                 open={editTodoModal}
