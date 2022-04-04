f96b50df5fe0f8528ad4547f17367b54a8a3ac40
M1NMAX <carlosaurora20@gmail.com>
2022-04-04 09:10:53 +0100

    upd event fetch

--- fetch/events/index.ts
+++ fetch/events/index.ts
@@ -12,3 +12,31 @@ export async function createEvent(collectionId: string, name: string, date: stri
     const res = await fetch(apiBaseUrl + '/events', requestOptions);
     return res.json().then(response => response.data);
 }
+
+export async function updateEvent(id: string, name: string, date: string, time: string, description: string, reminder: boolean): Promise<boolean> {
+    const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name, date, time, description, reminder })
+    };
+    const res = await fetch(apiBaseUrl + '/events/' + id, requestOptions);
+    return res.json().then(response => response.isSuccess);
+}
+
+
+export async function renameEvent(id: string, name: string): Promise<boolean> {
+    const requestOptions = {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name })
+    };
+    const res = await fetch(apiBaseUrl + '/events/' + id, requestOptions);
+    return res.json().then(response => response.isSuccess);
+}
+
+export async function deleteEvent(id: string): Promise<boolean> {
+    const res = await fetch(apiBaseUrl + '/events/' + id, { method: 'DELETE' });
+    return res.json().then(response => response.isSuccess);
+}
+
+
