7fa848445c12bc6d9488633499ca610c310c1eed
M1NMAX <carlosaurora20@gmail.com>
2022-05-13 18:41:02 +0100

    upd sidebar design

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,184 @@
+import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
+import ThemeBtn from '../ThemeBtn';
+import {
+  ChevronDoubleLeftIcon,
+  CollectionIcon,
+  PlusIcon,
+  SearchIcon,
+} from '@heroicons/react/outline';
+import SidebarBtn from './components/SidebarBtn';
+import ActionIcon from '../Frontstate/ActionIcon';
+import SidebarCollection from './components/SidebarCollection';
+import SidebarUserOptions from './components/SidebarUserOptions';
+import { sidebarState } from '../../atoms/sidebarAtom';
+import { useRecoilState } from 'recoil';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+import { CollectionInterface } from '../../interfaces';
+import { getUserCollections } from '../../fetch/collections';
+import { useQuery } from 'react-query';
+import { toast, Toaster } from 'react-hot-toast';
+import NewCollectionModal from '../NewCollectionModal';
+import { useRouter } from 'next/router';
+import Logo from '../Logo';
+
+const Sidebar: FC = () => {
+  const router = useRouter();
+
+  const { data, error, isError, isLoading } = useQuery<
+    CollectionInterface[],
+    Error
+  >('collections', getUserCollections);
+
+  const { width } = useWindowDimensions();
+  const [sidebar, setSidebar] = useRecoilState(sidebarState);
+
+  //Menu is open if window width > 768px, tailwind ´md´
+  useEffect(() => {
+    if (width > 768) setSidebar(true);
+  }, [width]);
+
+  const wrapper = useRef<HTMLDivElement>(null);
+  //handle outside click in small device
+  const checkOutsideClick = useCallback(
+    (event) => {
+      if (
+        sidebar &&
+        wrapper.current &&
+        !wrapper.current.contains(event.target) &&
+        width <= 768
+      ) {
+        setSidebar(false);
+      }
+    },
+    [sidebar, wrapper, width]
+  );
+
+  useEffect(() => {
+    document.addEventListener('mousedown', checkOutsideClick);
+    return () => document.removeEventListener('mousedown', checkOutsideClick);
+  }, [checkOutsideClick]);
+
+  //Modal: create collection
+  const [open, setOpen] = useState(false);
+  const closeModal = () => setOpen(false);
+  const openModal = () => setOpen(true);
+
+  const positiveFeedback = (msg: string) => toast.success(msg);
+  const negativeFeedback = () =>
+    toast.success('Something went wrong, try later');
+
+  const getSubCollection = (id: number): CollectionInterface[] => {
+    if (!data) return [];
+    return data.filter((collection) => collection.collectionId === id);
+  };
+
+  const handleCollection = (
+    idx: number,
+    collection: CollectionInterface
+  ): JSX.Element => {
+    let result = <></>;
+    if (!collection._id) return result;
+
+    if (collection.variant != 'collection') {
+      result = (
+        <SidebarCollection
+          key={idx}
+          id={collection._id}
+          name={collection.name}
+          variant={collection.variant}
+          isSub={false}
+        />
+      );
+    } else {
+      const subCollections = getSubCollection(collection._id);
+      result = (
+        <>
+          <SidebarCollection
+            key={idx}
+            id={collection._id}
+            name={collection.name}
+            variant={collection.variant}
+            isSub={false}
+          />
+          {subCollections.map((sub, i) => (
+            <SidebarCollection
+              key={i}
+              id={sub._id}
+              name={sub.name + 'smot'}
+              variant={sub.variant}
+              isSub={true}
+            />
+          ))}
+        </>
+      );
+    }
+
+    return result;
+  };
+
+  return (
+    <div
+      ref={wrapper}
+      className={`${sidebar ? 'w-3/4 sm:w-60' : 'w-0'} transition-all 
+        duration-200 ease-linear fixed top-0 left-0 z-10  h-screen  overflow-hidden
+        bg-gray-100 dark:bg-gray-800 dark:text-white`}>
+      <div className='flex flex-col px-1 py-2  space-y-1 '>
+        <div className='flex justify-between space-x-2'>
+          <button className='bg-gray-300 dark:bg-gray-600 w-full rounded-full'>
+            Find, explore, do
+          </button>
+          <ActionIcon
+            icon={<ChevronDoubleLeftIcon />}
+            variant='secondary'
+            onClick={() => setSidebar(false)}
+          />
+        </div>
+
+        <SidebarBtn icon={<SearchIcon />} text='Quick search' />
+
+        <div className='flex justify-between items-center'>
+          <SidebarBtn
+            icon={<CollectionIcon />}
+            text='Collections'
+            onClick={() => router.push('/collections')}
+          />
+          <ActionIcon
+            icon={<PlusIcon />}
+            variant='secondary'
+            onClick={openModal}
+          />
+        </div>
+
+        <div
+          className='flex flex-col space-y-0.5 sidebarCollections-height w-full overflow-y-auto overflow-x-hidden
+                scrollbar-thin scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-600 '>
+          {data?.map(
+            (collection, idx: number) =>
+              !collection.isSub && handleCollection(idx, collection)
+          )}
+          {isLoading && <span>Loading...</span>}
+          {isError && <span>Error: {error.message}</span>}
+        </div>
+
+        <div className='absolute bottom-1 w-full px-1 flex justify-between items-center '>
+          <SidebarUserOptions />
+          <ThemeBtn />
+        </div>
+      </div>
+      <Toaster />
+      {open && (
+        <NewCollectionModal
+          open={open}
+          isSub={false}
+          parentName=''
+          collectionId={null}
+          handleClose={closeModal}
+          positiveFeedback={positiveFeedback}
+          negativeFeedback={negativeFeedback}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Sidebar;
--- components/Sidebar/components/SidebarUserOptions/index.tsx
+++ components/Sidebar/components/SidebarUserOptions/index.tsx
@@ -3,80 +3,82 @@ import Image from 'next/image';
 import { useUser } from '@auth0/nextjs-auth0';
 import { CogIcon, LogoutIcon, SelectorIcon } from '@heroicons/react/outline';
 import logoSrc from '../../../../public/logo192.png';
-import { Menu, Transition } from '@headlessui/react'
+import { Menu, Transition } from '@headlessui/react';
 
-const SidebarUserOptions: FC = ({ }) => {
-    const { user, error, isLoading } = useUser();
+const SidebarUserOptions: FC = ({}) => {
+  const { user, error, isLoading } = useUser();
 
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>{error.message}</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
-    return (
-        <Menu >
-            <Menu.Button className='space-x-2 btn btn-secondary grow truncate'>
-                <div className="relative w-6 h-6 ">
-                    <Image src={user?.picture || logoSrc} layout='fill' objectFit='contain' className='rounded-full' />
+  return (
+    <Menu>
+      <Menu.Button className='space-x-2 btn btn-secondary grow truncate'>
+        <div className='relative w-8 h-8'>
+          <Image
+            src={user?.picture || logoSrc}
+            layout='fill'
+            objectFit='contain'
+            className=' p-2 rounded-full border-2 border-white'
+          />
+        </div>
+        <div className='flex flex-col items-start'>
+          <span className='font-medium text-sm'>{user?.nickname}</span>
+          <span className='text-xs'>Master</span>
+        </div>
+      </Menu.Button>
+
+      <Transition
+        as={Fragment}
+        enter='transition ease-out duration-100'
+        enterFrom='transform opacity-0 scale-95'
+        enterTo='transform opacity-100 scale-100'
+        leave='transition ease-in duration-75'
+        leaveFrom='transform opacity-100 scale-100'
+        leaveTo='transform opacity-0 scale-95'>
+        <div className='fixed z-10'>
+          <Menu.Items
+            as='ul'
+            className='absolute left-4 w-56 origin-top-right bg-gray-200 dark:bg-gray-900 divide-y divide-gray-100
+                            rounded-sm shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+            <div className='px-1 py-1 space-y-1 font-medium '>
+              <Menu.Item as='li'>
+                <div className='flex items-center space-x-2 px-1.5'>
+                  <div className='relative w-10 h-10 '>
+                    <Image
+                      src={user?.picture || logoSrc}
+                      layout='fill'
+                      objectFit='contain'
+                      className='rounded-full'
+                    />
+                  </div>
+                  <div className='flex flex-col items-start font-medium '>
+                    <span>{user?.nickname}</span>
+                    <span className='text-xs'>{user?.email}</span>
+                  </div>
                 </div>
-                <div className='flex items-center font-medium  '>
-                    {user?.nickname}
-                    <SelectorIcon className='icon-xs' />
-                </div>
-            </Menu.Button>
+              </Menu.Item>
+              <hr className='bg-gray-500 dark:bg-gray-700' />
+              <Menu.Item as='li'>
+                <a href='/settings' className='btn btn-secondary space-x-1'>
+                  <CogIcon className='icon-md' />
+                  <span>Settings</span>
+                </a>
+              </Menu.Item>
+              <Menu.Item>
+                <a
+                  href='/api/auth/logout'
+                  className='btn btn-secondary space-x-1'>
+                  <LogoutIcon className='icon-md' />
+                  <span>Log out</span>
+                </a>
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </div>
+      </Transition>
+    </Menu>
+  );
+};
 
-            <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95">
-
-                <div className='fixed z-10'>
-                    <Menu.Items as='ul' className="absolute left-4 w-56 origin-top-right bg-gray-200 dark:bg-gray-900 divide-y divide-gray-100
-                            rounded-sm shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <div className="px-1 py-1 space-y-1 font-medium ">
-                            <Menu.Item as='li'>
-                                <div className='flex items-center space-x-2 px-1.5'>
-                                    <div className="relative w-10 h-10 ">
-                                        <Image src={user?.picture || logoSrc} layout='fill' objectFit='contain' className='rounded-full' />
-                                    </div>
-                                    <div className='flex flex-col items-start font-medium '>
-                                        <span>
-                                            {user?.nickname}
-                                        </span>
-                                        <span className='text-xs'>
-                                            {user?.email}
-                                        </span>
-                                    </div>
-                                </div>
-                            </Menu.Item>
-                            <hr className='bg-gray-500 dark:bg-gray-700' />
-                            <Menu.Item as='li'>
-                                <a href="/settings"
-                                    className='btn btn-secondary space-x-1'>
-                                    <CogIcon className='icon-md' />
-                                    <span>
-                                        Settings
-                                    </span>
-                                </a>
-                            </Menu.Item>
-                            <Menu.Item>
-                                <a href="/api/auth/logout"
-                                    className='btn btn-secondary space-x-1'>
-                                    <LogoutIcon className='icon-md' />
-                                    <span>
-                                        Log out
-                                    </span>
-                                </a>
-                            </Menu.Item>
-                        </div>
-
-                    </Menu.Items>
-                </div>
-            </Transition>
-        </Menu>
-    );
-}
-
-export default SidebarUserOptions
+export default SidebarUserOptions;
