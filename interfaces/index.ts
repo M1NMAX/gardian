899ccb0bed603755368c00ce94b13200917116c9
M1NMAX <carlosaurora20@gmail.com>
2022-05-21 13:04:20 +0100

    added template api

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -30,72 +30,7 @@ export interface ICollection extends IBase {
     items: IItem[],
 }
 
-
-//DELETE
-//Schemas
-interface basicSchema {
-    _id?: number,
-    name: string,
-    userId: string,
-    collectionId: Number | null,
-    createdAt?: Date,
-    updatedAt?: Date,
-}
-
-export interface PropertyInCollectionInterface {
-    _id?: number,
-    name: string,
-    type: string,
-    values: string[]
-}
-
-export interface PropertyInItemInterface {
-    _id?: number,
-    name: string,
-    value: string,
-}
-
-export interface CollectionInterface extends basicSchema {
-    variant: string,
-    isSub: boolean,
-    properties: PropertyInCollectionInterface[],
-    description: string,
-    isDescriptionHidden: boolean,
-    parentName: string,
-}
-
-export interface CustomItemInterface extends basicSchema {
-    properties: PropertyInItemInterface[],
-}
-
-export interface EventInterface extends basicSchema {
-    date: string,
-    time: string,
-    reminder: boolean,
-    description: string,
-}
-
-
-export interface DocumentInterface extends basicSchema {
-    content: string
-}
-
-export interface TodoInterface extends basicSchema {
-    isConcluded: boolean,
-    conclusionDate?: string,
-    reminderDate?: string,
-    description?: string,
-}
-
 //Components props
-
-export interface CollectionOverviewProps {
-    collection: CollectionInterface,
-}
-
-
-
-
 export interface ModalProps {
     open: boolean,
     handleClose: (value?: boolean | React.MouseEvent<HTMLButtonElement>) => void,
