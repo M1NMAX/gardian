86e15c98d8640b0c3154dca6905b05bdb44f9050
M1NMAX <carlosaurora20@gmail.com>
2022-05-24 09:36:10 +0100

    added templates page

---
+++ pages/templates/index.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { InferGetServerSidePropsType, NextPage } from 'next';
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import Sidebar from '../../components/Sidebar';
+import Head from 'next/head';
+import { useQuery } from 'react-query';
+import { getUserCollections } from '../../fetch/collections';
+import CollectionOverview from '../../components/CollectionOverview/CollectionOverview';
+import { useRecoilState } from 'recoil';
+import { sidebarState } from '../../atoms/sidebarAtom';
+import ActionIcon from '../../components/Frontstate/ActionIcon';
+import { MenuAlt2Icon } from '@heroicons/react/outline';
+import toast, { Toaster } from 'react-hot-toast';
+import NewCollectionModal from '../../components/NewCollectionModal';
+
+const TemplatesPage: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ user }) => {
+  const [sidebar, setSidebar] = useRecoilState(sidebarState);
+
+  //TODO: Add loading and error
+
+  //Modal: create collection
+  const [open, setOpen] = useState(false);
+  const closeModal = () => setOpen(false);
+  const openModal = () => setOpen(true);
+
+  const positiveFeedback = (msg: string) => toast.success(msg);
+  const negativeFeedback = () =>
+    toast.success('Something went wrong, try later');
+
+  return (
+    <>
+      <Head>
+        <title>Collections</title>
+      </Head>
+      <Sidebar />
+      <main
+        className={`${
+          sidebar ? 'w-full md:has-sidebar-width md:ml-60' : 'w-full'
+        } main-content`}>
+        <h1 className='text-lg'>heoo</h1>
+      </main>
+      <Toaster />
+    </>
+  );
+};
+
+export default TemplatesPage;
+
+export const getServerSideProps = withPageAuthRequired();
