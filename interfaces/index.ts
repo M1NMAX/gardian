e1329006c3ec83c22c5912df8905ed555667c634
M1NMAX <carlosaurora20@gmail.com>
2022-06-06 16:08:57 +0100

    refactor property types and added new one

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,59 +1,58 @@
+import { PropertyType } from '../types';
+
 //Base schema
 interface IBase {
-    _id?: number,
-    name: string,
-    createdAt?: Date,
-    updatedAt?: Date,
+  _id?: number;
+  name: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 export interface IGroup extends IBase {
-    collections: Number[] | ICollection[]
-    userId: string,
+  collections: Number[] | ICollection[];
+  userId: string;
 }
 
 export interface IProperty extends IBase {
-    name: string,
-    type: "text" | "select" | "date" | "time" | "url",
-    values: string[],
-    color: string,
+  name: string;
+  type: PropertyType;
+  values: string[];
+  color: string;
 }
 export interface ITemplate extends IBase {
-    properties: IProperty[],
-    description?: string,
+  properties: IProperty[];
+  description?: string;
 }
-
 
 export interface IItemProperty extends IBase {
-    value: string,
+  value: string;
 }
 export interface IItem extends IBase {
-    properties: IItemProperty[],
+  properties: IItemProperty[];
 }
 
 export interface ICollection extends IBase {
-    userId?: string | null,
-    description: string,
-    template?: ITemplate,
-    items?: Number[] | IItem[],
+  userId?: string | null;
+  description: string;
+  template?: ITemplate;
+  items?: Number[] | IItem[];
 }
 
-
-//Item example for template 
+//Item example for template
 export interface IExample extends IBase {
-    items: IItem[],
-    templateId: number
+  items: IItem[];
+  templateId: number;
 }
 
 //Components props
 export interface ModalProps {
-    open: boolean,
-    handleClose: (value?: boolean | React.MouseEvent<HTMLButtonElement>) => void,
-    positiveFeedback: (value: string) => void,
-    negativeFeedback: () => void,
+  open: boolean;
+  handleClose: (value?: boolean | React.MouseEvent<HTMLButtonElement>) => void;
+  positiveFeedback: (value: string) => void;
+  negativeFeedback: () => void;
 }
 
-
 export interface DocumentStatusProps {
-    isSaved: boolean,
-    isError: boolean,
+  isSaved: boolean;
+  isError: boolean;
 }
