221bd6778b842036094a8aff8437dd7026e8549a
M1NMAX <carlosaurora20@gmail.com>
2022-06-10 20:30:10 +0100

    upd: group api and added new fetch functions

--- fetch/group/index.ts
+++ fetch/group/index.ts
@@ -1,24 +1,54 @@
-import { IGroup } from "../../interfaces";
+import { IGroup } from '../../interfaces';
+import { getRequestOptions } from '../utils';
 
+const baseUrl = process.env.NEXT_PUBLIC_API_URL + '/groups/';
 
-const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL;
-
-export async function createGroup(name: string,): Promise<IGroup> {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name })
-    };
-    const res = await fetch(apiBaseUrl + '/groups', requestOptions);
-    return res.json().then(response => response.data);
+export async function createGroup(name: string): Promise<IGroup> {
+  const res = await fetch(baseUrl, getRequestOptions('POST', { name }));
+  return res.json().then((response) => response.data);
 }
 
-
 export async function getGroups(): Promise<IGroup[]> {
-    const res = await fetch(apiBaseUrl + '/groups');
-    return res.json().then(response => response.data);
-} 
+  const res = await fetch(baseUrl);
+  return res.json().then((response) => response.data);
+}
 export async function getGroup(id: number): Promise<IGroup> {
-    const res = await fetch(apiBaseUrl + '/groups/' + id);
-    return res.json().then(response => response.data);
+  const res = await fetch(baseUrl + id);
+  return res.json().then((response) => response.data);
+}
+
+export async function updateGroup(id: number, group: IGroup): Promise<boolean> {
+  const res = await fetch(baseUrl + id, getRequestOptions('PUT', { group }));
+  return res.json().then((response) => response.isSuccess);
+}
+
+export async function deleteGroup(id: number): Promise<boolean> {
+  const res = await fetch(baseUrl + id, { method: 'DELETE' });
+  return res.json().then((response) => response.isSuccess);
+}
+
+export async function renameGroup(id: number, name: string): Promise<boolean> {
+  const group = await getGroup(id);
+  return updateGroup(id, { ...group, name });
+}
+
+export async function addCollectionToGroup(
+  groupId: number,
+  collectionId: number
+): Promise<boolean> {
+  const res = await fetch(
+    baseUrl + groupId + '/' + collectionId,
+    getRequestOptions('PUT', {})
+  );
+  return res.json().then((response) => response.isSuccess);
+}
+
+export async function removeCollectionFromGroup(
+  groupId: number,
+  collectionId: number
+): Promise<boolean> {
+  const res = await fetch(baseUrl + groupId + '/' + collectionId, {
+    method: 'DELETE',
+  });
+  return res.json().then((response) => response.isSuccess);
 }
