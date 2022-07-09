7ec8089fc6850fdbdb22c453a79ae9d4a91831e3
M1NMAX <carlosaurora20@gmail.com>
2022-07-09 08:32:21 +0100

    upd collection overview design

--- components/CollectionOverview/CollectionOverview.tsx
+++ components/CollectionOverview/CollectionOverview.tsx
@@ -1,3 +1,4 @@
+import { LightningBoltIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 import React, { FC } from 'react';
 import { ICollection } from '../../interfaces';
@@ -8,15 +9,25 @@ interface CollectionOverviewProps {
 
 const CollectionOverview: FC<CollectionOverviewProps> = (props) => {
   const { collection } = props;
+  const { _id: id, name, properties, items } = collection;
 
   return (
-    <Link href={`/collections/${collection._id}`}>
+    <Link href={`/collections/${id}`}>
       <a className='flex flex-col p-1 rounded shadow-md bg-gray-100 dark:bg-gray-800 '>
-        <span className=' font-semibold text-lg'>{collection.name}</span>
-        <span className='text-xs font-light italic'>
-          {collection.updatedAt
-            ? new Date(collection.updatedAt).toDateString()
-            : 'Loading'}
+        <span className=' font-semibold text-lg'>{name}</span>
+
+        <span className='w-full grid grid-flow-col auto-cols-max gap-0.5 md:gap-1 text-sm overflow-x-auto scrollbar-none'>
+          {properties.map((property) => (
+            <span className=' text-sm font-semibold px-1 rounded bg-white dark:bg-gray-700 '>
+              {property.name}
+            </span>
+          ))}
+          {properties.length === 0 && 'N/A'}
+        </span>
+
+        <span className='mt-0.5 flex items-center space-x-0.5'>
+          <LightningBoltIcon className='w-4 h-4' />
+          <span className='text-xs font-light italic'>{items.length}</span>
         </span>
       </a>
     </Link>
