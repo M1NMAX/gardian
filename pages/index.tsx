ba73f6f131daf8666fbe39a4b2d968583cb71f4a
M1NMAX <carlosaurora20@gmail.com>
2022-07-12 14:43:56 +0100

    upd home page ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
-import { getSession } from '@auth0/nextjs-auth0'
-import type { GetServerSidePropsContext, NextPage } from 'next'
-import Head from 'next/head'
-import Header from '../components/Header'
+import { getSession } from '@auth0/nextjs-auth0';
+import type { GetServerSidePropsContext, NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import Header from '../components/Header';
+import collecting from '../public/undraw_collecting.svg';
+import template from '../public/undraw_wireframing.svg';
+import nextjsLogo from '../public/next-js-logo.png';
 
 const Home: NextPage = () => {
   return (
@@ -10,15 +15,60 @@ const Home: NextPage = () => {
         <title>Gardian</title>
       </Head>
       <Header />
-      <div className='text-red-500 uppercase text-center font-bold'>
-        <h1>hELLO</h1>
+      <main className='space-y-4 dark:bg-gray-700'>
+        <div className='w-screen md:max-w-5xl mx-auto py-8 flex items-center'>
+          <div className='basis-1/2 space-y-4 flex flex-col'>
+            <p className='text-3xl font-bold uppercase dark:text-white'>
+              Home <br /> for all &nbsp;
+              <span className='text-green-400'>collections</span>
+            </p>
+            <p className='text-2xl font-medium text-gray-500 dark:text-gray-300'>
+              Gardian let you store your items in collections
+            </p>
+            <Link href='/api/auth/login'>
+              <a
+                className='w-full max-w-xs  font-medium text-center text-gray-100 p-1 rounded 
+              bg-green-500 hover:bg-green-400'>
+                Try Gardian Free
+              </a>
+            </Link>
+          </div>
 
-      </div>
+          <div className='basis-1/2'>
+            <Image src={collecting} objectFit='contain' alt='collecting' />
+          </div>
+        </div>
+
+        <div className='w-screen md:max-w-lg mx-auto my-8 flex flex-col items-center'>
+          <div>
+            <p className='text-2xl font-bold dark:text-gray-100'>
+              No need to start from scratch.
+            </p>
+            <p className='text-2xl font-bold dark:text-gray-100'>
+              Your collection can start with a template.
+            </p>
+          </div>
+
+          <div className='p-4'>
+            <Image src={template} objectFit='contain' alt='wireframing' />
+          </div>
+        </div>
+      </main>
+      <footer className='py-10 flex flex-col items-center dark:bg-gray-900 dark:text-white'>
+        <p>Copyright &copy; 2022</p>
+        <p>
+          Created with{' '}
+          <span className='px-0.5 rounded-md dark:bg-white'>
+            <Image src={nextjsLogo} height={15} width={15} alt='Nextjs logo' />
+          </span>
+          by M1NMAX
+        </p>
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   const sesssion = getSession(ctx.req, ctx.res);
@@ -30,8 +80,7 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
         destination: '/collections',
         permanent: false,
       },
-    }
+    };
   }
-  return { props: {} as never }
-
+  return { props: {} as never };
 }
