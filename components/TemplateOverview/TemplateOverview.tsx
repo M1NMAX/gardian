ddb0105d18c346cf3a45c3a089159136d55cb2fd
M1NMAX <carlosaurora20@gmail.com>
2022-08-20 12:11:48 +0100

    upd: turn templateOverview responsive

--- components/TemplateOverview/TemplateOverview.tsx
+++ components/TemplateOverview/TemplateOverview.tsx
@@ -1,18 +1,20 @@
+import { TemplateIcon } from '@heroicons/react/outline';
 import React, { FC } from 'react';
 import { ITemplate } from '../../interfaces';
 
 interface TemplateOverviewProps {
   template: ITemplate;
   active: boolean;
-  onTemplateClick: (id: number) => void;
+  view: string;
+  onClickTemplate: (id: string) => void;
 }
 const TemplateOverview: FC<TemplateOverviewProps> = (props) => {
-  const { template, active, onTemplateClick } = props;
-  const { _id: id, name, properties } = template;
+  const { template, active, view, onClickTemplate } = props;
+  const { _id: id, name, description, properties } = template;
 
   const handleClick = () => {
     if (!id) return;
-    onTemplateClick(id);
+    onClickTemplate(id);
   };
 
   return (
@@ -20,9 +22,13 @@ const TemplateOverview: FC<TemplateOverviewProps> = (props) => {
       onClick={handleClick}
       className={`${
         active && 'border-r-2 border-green-500'
-      }  flex flex-col p-1 rounded shadow-md bg-gray-100 dark:bg-gray-800 
+      }  flex flex-col p-1 space-y-[1px] rounded shadow-md bg-gray-100 dark:bg-gray-800 
       `}>
-      <span className=' font-semibold text-lg'>{name}</span>
+      <span className='grow flex items-center space-x-1'>
+        <TemplateIcon className='icon-xs' />
+        <span className='grow font-semibold text-lg'>{name}</span>
+      </span>
+
       <span className='w-full grid grid-flow-col auto-cols-max gap-0.5 md:gap-1 text-sm overflow-x-auto scrollbar-none'>
         {properties.map((property, idx) => (
           <span
@@ -32,6 +38,7 @@ const TemplateOverview: FC<TemplateOverviewProps> = (props) => {
           </span>
         ))}
       </span>
+      {view === 'list' && <span className='text-left'>{description}</span>}
     </button>
   );
 };
