27ecc9ff126e638745aee07ec0dfb1687633636e
M1NMAX <carlosaurora20@gmail.com>
2022-08-22 12:46:35 +0100

    revert: move Header and Sidebar to layout folder

---
+++ layouts/Header/Header.tsx
@@ -0,0 +1,34 @@
+import { MenuAlt2Icon } from '@heroicons/react/outline';
+import React, { FC, ReactNode } from 'react';
+import { ActionIcon } from '../../components/frontstate-ui';
+
+interface HeaderProps {
+  title: string;
+  sidebar: boolean;
+  children: ReactNode;
+  onClickMenuBtn: () => void;
+}
+
+const Header: FC<HeaderProps> = (props) => {
+  const { title, sidebar, children, onClickMenuBtn } = props;
+  return (
+    <div className='sticky top-0 pt-2 px-4 bg-white dark:bg-gray-900 dark:text-white'>
+      <div className='space-y-1 pb-1 border-dotted border-b-2 border-gray-200 dark:border-gray-700'>
+        {/* Menu btn  */}
+        {!sidebar && (
+          <ActionIcon icon={<MenuAlt2Icon />} onClick={onClickMenuBtn} />
+        )}
+
+        {/* Title   */}
+        <div className='flex items-center space-x-1'>
+          <h1 className='grow font-semibold text-xl md:text-2xl pl-1 border-l-4 border-primary-100'>
+            {title}
+          </h1>
+          <div className='flex items-center space-x-1.5'>{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ layouts/Sidebar/SidebarGroup/SidebarGroup.tsx
@@ -0,0 +1,130 @@
+import { Disclosure } from '@headlessui/react';
+import { ChevronRightIcon, LightningBoltIcon } from '@heroicons/react/outline';
+import React, { FC, ReactNode } from 'react';
+import toast from 'react-hot-toast';
+import { useMutation, useQueryClient } from 'react-query';
+import { deleteGroup, renameGroup } from '../../../fetch/group';
+import useModal from '../../../hooks/useModal';
+import { IGroup } from '../../../interfaces';
+import DeleteModal from '../../../components/DeleteModal';
+import RenameModal from '../../../components/RenameModal';
+import SidebarGroupMenu from '../SidebarGroupMenu';
+
+interface SidebarGroupProps {
+  children: ReactNode;
+  group: IGroup;
+}
+
+const SidebarGroup: FC<SidebarGroupProps> = (props) => {
+  const { children, group } = props;
+  const { _id: id, name } = group;
+
+  //Feedbacks
+  const positiveFeedback = (msg: string) => toast.success(msg);
+  const negativeFeedback = () =>
+    toast.success('Something went wrong, try later');
+
+  //Modals
+  const renameGroupModal = useModal();
+  const deleteGroupModal = useModal();
+
+  const queryClient = useQueryClient();
+
+  //handle rename Group and its mutation
+  const renameGroupMutation = useMutation(
+    async (name: string) => {
+      if (!id) return;
+      await renameGroup(id, name);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['groups']);
+
+        positiveFeedback('Success');
+      },
+      onError: () => {
+        negativeFeedback();
+      },
+      onSettled: () => {
+        renameGroupModal.closeModal();
+      },
+    }
+  );
+
+  //handle delete group and its mutation
+  const deleteGroupMutation = useMutation(
+    async () => {
+      if (!id) return;
+      await deleteGroup(id);
+    },
+    {
+      onSuccess: () => {
+        positiveFeedback('Success');
+        queryClient.invalidateQueries(['groups']);
+      },
+      onError: () => {
+        negativeFeedback();
+      },
+      onSettled: () => {
+        deleteGroupModal.closeModal();
+      },
+    }
+  );
+
+  return (
+    <>
+      <Disclosure
+        defaultOpen
+        as='div'
+        className='rounded bg-gray-200  dark:bg-gray-700'>
+        {({ open }) => (
+          <>
+            <div className='group flex items-center justify-between py-0.5 pr-2'>
+              <Disclosure.Button className='flex items-center space-x-0.5 w-full p-0.5'>
+                <ChevronRightIcon
+                  className={`${open ? 'rotate-90 transform' : ''} icon-xs`}
+                />
+                <span> {name}</span>
+              </Disclosure.Button>
+              {/* Only show delete btn if group empty */}
+
+              <span className='block md:hidden md:group-hover:block md:group-focus-within:block'>
+                <SidebarGroupMenu
+                  showDelete={group.collections.length === 0}
+                  onClickRename={renameGroupModal.openModal}
+                  onClickDelete={deleteGroupModal.openModal}
+                />
+              </span>
+            </div>
+            {/*Display group collections */}
+            {group.collections.length > 0 && (
+              <Disclosure.Panel className='py-1 text-sm'>
+                {children}
+              </Disclosure.Panel>
+            )}
+          </>
+        )}
+      </Disclosure>
+
+      {renameGroupModal.isOpen && (
+        <RenameModal
+          open={renameGroupModal.isOpen}
+          handleClose={renameGroupModal.closeModal}
+          name={name}
+          onRename={renameGroupMutation.mutate}
+        />
+      )}
+
+      {deleteGroupModal.isOpen && (
+        <DeleteModal
+          open={deleteGroupModal.isOpen}
+          handleClose={deleteGroupModal.closeModal}
+          name={name}
+          onDelete={deleteGroupMutation.mutate}
+        />
+      )}
+    </>
+  );
+};
+
+export default SidebarGroup;
