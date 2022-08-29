beed30ec6fa8aca68d7d9c619b5302f06d750b30
M1NMAX <carlosaurora20@gmail.com>
2022-08-29 10:43:08 +0100

    refactor: Button, new variant and isDisable as prop

--- components/frontstate-ui/Button/Button.tsx
+++ components/frontstate-ui/Button/Button.tsx
@@ -5,12 +5,14 @@ type ButtonVariantsType =
   | 'primary-filled'
   | 'primary-hover'
   | 'secondary-filled'
-  | 'secondary-hover';
+  | 'secondary-hover'
+  | 'danger-filled';
 
 interface ButtonProps {
   variant?: ButtonVariantsType;
   type?: 'button' | 'submit' | 'reset';
   full?: boolean;
+  isDisabled?: boolean;
   onClick?: (ev: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
@@ -20,11 +22,13 @@ const Button: FC<ButtonProps> = (props) => {
     variant = 'secondary-filled',
     type = 'button',
     full = false,
+    isDisabled = false,
     onClick,
   } = props;
   return (
     <button
       type={type}
+      disabled={isDisabled}
       onClick={onClick}
       className={`${style.btn} ${style[variant]} ${full && style.full}`}>
       {children}
