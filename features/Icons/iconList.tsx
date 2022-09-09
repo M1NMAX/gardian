e74f3184f7a8faf00b6b2958b4d644026864efde
M1NMAX <carlosaurora20@gmail.com>
2022-09-09 09:46:35 +0100

    add: more icon to iconList

--- features/Icons/iconList.tsx
+++ features/Icons/iconList.tsx
@@ -1,5 +1,19 @@
 import {
   AcademicCapIcon,
+  AdjustmentsHorizontalIcon,
+  ArchiveBoxIcon,
+  BanknotesIcon,
+  Battery0Icon,
+  Battery100Icon,
+  Battery50Icon,
+  BeakerIcon,
+  BellAlertIcon,
+  BellIcon,
+  BellSlashIcon,
+  BellSnoozeIcon,
+  BoltIcon,
+  BoltSlashIcon,
+  BookOpenIcon,
   CalendarDaysIcon,
   CalendarIcon,
   CheckBadgeIcon,
@@ -28,6 +42,62 @@ const IconList: IIcons = {
   AcademicCapIcon: {
     component: <AcademicCapIcon />,
     keywords: ['academic', 'cap'],
+  },
+  AdjustmentsHorizontalIcon: {
+    component: <AdjustmentsHorizontalIcon />,
+    keywords: ['adjustment', 'settings'],
+  },
+  ArchiveBoxIcon: {
+    component: <ArchiveBoxIcon />,
+    keywords: ['archive', 'box'],
+  },
+  BanknotesIcon: {
+    component: <BanknotesIcon />,
+    keywords: ['bank', 'note', 'money'],
+  },
+  Battery0Icon: {
+    component: <Battery0Icon />,
+    keywords: ['battery', 'charge', '0'],
+  },
+  Battery50Icon: {
+    component: <Battery50Icon />,
+    keywords: ['battery', 'charge', '50', 'half'],
+  },
+  Battery100Icon: {
+    component: <Battery100Icon />,
+    keywords: ['battery', 'charge', '100', 'full'],
+  },
+  BeakerIcon: {
+    component: <BeakerIcon />,
+    keywords: ['beaker', 'chemistry'],
+  },
+  BellAlertIcon: {
+    component: <BellAlertIcon />,
+    keywords: ['bell', 'alert', 'notification'],
+  },
+  BellSlashIcon: {
+    component: <BellSlashIcon />,
+    keywords: ['bell', 'mute', 'notification'],
+  },
+  BellSnoozeIcon: {
+    component: <BellSnoozeIcon />,
+    keywords: ['bell', 'snooze', 'alarm'],
+  },
+  BellIcon: {
+    component: <BellIcon />,
+    keywords: ['bell', 'alarm'],
+  },
+  BoltIcon: {
+    component: <BoltIcon />,
+    keywords: ['bolt', 'flash'],
+  },
+  BoltSlashIcon: {
+    component: <BoltSlashIcon />,
+    keywords: ['bolt', 'slash'],
+  },
+  BookOpenIcon: {
+    component: <BookOpenIcon />,
+    keywords: ['book', 'open', 'read'],
   },
   CalendarDaysIcon: {
     component: <CalendarDaysIcon />,
